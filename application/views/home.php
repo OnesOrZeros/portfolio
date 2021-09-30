0cca8efa83ed506e33e6a2421ff25a255a78f689
OnesOrZeros <dyfreezymintytursh@gmail.com>
2021-09-30 13:36:36 +0300

    edits

--- application/views/home.php
+++ application/views/home.php
@@ -43,7 +43,7 @@
                         <!-- main title -->
                         <div class="art-banner-title">
                           <!-- title -->
-                          <h1 class="mb-15">Discover my Amazing <br>Development Space!</h1>
+                          <h1 class="mb-15">Changing the World  <br>With Each Line of Code.</h1>
                           <!-- suptitle -->
                           <div class="art-lg-text art-code mb-25">&lt;<i>code</i>&gt; I build <span class="txt-rotate" data-period="2000"
                               data-rotate='[ "web interfaces.", "great software.", "android applications.", "design mocups.", "automation tools." ]'></span>&lt;/<i>code</i>&gt;</div>
@@ -54,7 +54,7 @@
                         </div>
                         <!-- main title end -->
                         <!-- photo -->
-                        <img src="<?php echo base_url();?>/resources/img/face-2.png" class="art-banner-photo" alt="Your Name">
+                        <img src="<?php echo base_url();?>/resources/img/prof1.jpg" class="art-banner-photo" alt="Your Name">
                       </div>
                       <!-- banner overlay end -->
                     </div>
@@ -139,11 +139,11 @@
                       <!-- counter -->
                       <div class="art-counter-box">
                         <!-- counter number -->
-                        <span class="art-counter">10</span><span class="art-counter-plus">+</span>
+                        <span class="art-counter">100000</span><span class="art-counter-plus">s</span>
                       </div>
                       <!-- counter end -->
                       <!-- title -->
-                      <h6>Honors and Awards</h6>
+                      <h6>Lines of Code</h6>
                     </div>
                     <!-- couner frame end -->
 
@@ -312,7 +312,146 @@
               <!-- container end -->
 
 
+              <!-- container -->
+              <div class="container-fluid">
 
+                <!-- row -->
+                <div class="row p-0-0">
+
+                  <!-- col -->
+                  <div class="col-lg-12">
+
+                    <!-- section title -->
+                    <div class="art-section-title">
+                      <!-- title frame -->
+                      <div class="art-title-frame">
+                        <!-- title -->
+                        <h4>Price Plans</h4>
+                      </div>
+                      <!-- title frame end -->
+                    </div>
+                    <!-- section title end -->
+
+                  </div>
+                  <!-- col end -->
+
+                  <!-- col -->
+                  <div class="col-lg-4">
+
+                    <!-- price -->
+                    <div class="art-a art-price">
+                      <!-- price body -->
+                      <div class="art-price-body">
+                        <h5 class="mb-30">Starter Price</h5>
+                        <!-- price cost -->
+                        <div class="art-price-cost">
+                          <div class="art-number">FREE<sup>*</sup></div>
+                        </div>
+                        <!-- price cost end -->
+                        <!-- price list -->
+                        <ul class="art-price-list">
+                          <!-- list item -->
+                          <li>Ui Design</li>
+                          <!-- list item -->
+                          <li>Web Development</li>
+                          <!-- list item -->
+                          <li class="art-empty-item">Logo design</li>
+                          <!-- list item -->
+                          <li class="art-empty-item">SEO optimization</li>
+                          <!-- list item -->
+                          <li class="art-empty-item">Wordpress integration</li>
+                        </ul>
+                        <!-- price list end -->
+                        <!-- button -->
+                        <a href="/contact.html" class="art-link art-color-link art-w-chevron">Order now</a>
+                        <div class="art-asterisk"><sup>*</sup>Free only when ordering paid services</div>
+                      </div>
+                      <!-- price body end -->
+                    </div>
+                    <!-- price end -->
+
+                  </div>
+                  <!-- grid -->
+
+                  <!-- col -->
+                  <div class="col-lg-4">
+
+                    <!-- price -->
+                    <div class="art-a art-price art-popular-price">
+                      <!-- price body -->
+                      <div class="art-price-body">
+                        <h5 class="mb-30">Hourly payment</h5>
+                        <!-- price cost -->
+                        <div class="art-price-cost">
+                          <div class="art-number"><span>$</span>29<span>h</span></div>
+                        </div>
+                        <!-- price cost end -->
+                        <!-- price list -->
+                        <ul class="art-price-list">
+                          <!-- list item -->
+                          <li>Ui Design</li>
+                          <!-- list item -->
+                          <li>Web Development</li>
+                          <!-- list item -->
+                          <li>Logo design</li>
+                          <!-- list item -->
+                          <li class="art-empty-item">SEO optimization</li>
+                          <!-- list item -->
+                          <li class="art-empty-item">Wordpress integration</li>
+                        </ul>
+                        <!-- price list end -->
+                        <!-- button -->
+                        <a href="/contact.html" class="art-link art-color-link art-w-chevron">Order now</a>
+                      </div>
+                      <!-- price body end -->
+                    </div>
+                    <!-- price end -->
+
+                  </div>
+                  <!-- col end -->
+
+                  <!-- col -->
+                  <div class="col-lg-4">
+
+                    <!-- price -->
+                    <div class="art-a art-price">
+                      <!-- price body -->
+                      <div class="art-price-body">
+                        <h5 class="mb-30"> Full time</h5>
+                        <!-- price cost -->
+                        <div class="art-price-cost">
+                          <div class="art-number"><span>$</span>2999<span>m</span></div>
+                        </div>
+                        <!-- price cost end -->
+                        <!-- price list -->
+                        <ul class="art-price-list">
+                          <!-- list item -->
+                          <li>Ui Design</li>
+                          <!-- list item -->
+                          <li>Web Development</li>
+                          <!-- list item -->
+                          <li>Logo design</li>
+                          <!-- list item -->
+                          <li>SEO optimization</li>
+                          <!-- list item -->
+                          <li>Wordpress integration</li>
+                        </ul>
+                        <!-- price list end -->
+                        <!-- button -->
+                        <a href="/contact.html" class="art-link art-color-link art-w-chevron">Order now</a>
+                      </div>
+                      <!-- price body end -->
+                    </div>
+                    <!-- price end -->
+
+                  </div>
+                  <!-- col end -->
+
+                </div>
+                <!-- row end -->
+
+              </div>
+              <!-- container end -->
 
 
               <!-- container -->
