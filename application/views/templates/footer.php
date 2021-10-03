f85f63db9c6ef7dffd939dab8b308a6cc019e82b
Marichtech <54105474+MarichTech@users.noreply.github.com>
2021-10-03 11:32:30 +0300

    footer update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -64,19 +64,14 @@
                   <li class="menu-item"><a href="<?php echo base_url(); ?>history">History</a></li>
                   <!-- menu item -->
                   <li class="menu-item"><a href="<?php echo base_url(); ?>contact">Contact</a></li>
-<<<<<<< HEAD
+
                   <!-- menu item -->
                  
-<<<<<<< HEAD
-                
-=======
+
                   <!-- menu item 
                   <li class="menu-item"><a href="<?php echo base_url(); ?>onepage" target="_blank">Onepage</a></li>
                   -->
->>>>>>> kevo
-=======
-                  
->>>>>>> fred
+
                 </ul>
                 <!-- menu list end -->
               </nav>
